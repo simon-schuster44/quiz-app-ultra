9fe74c5b0fa40bdad7c181e4cc492e5e84ce0d74
Simon Schuster <simschu@gmx.de>
2022-11-02 20:29:39 +0100

    solved part 2 challenges

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import "./App.css";
 import Header from "./Components/Header/Header.js";
-import "./Components/Header/Header.css";
 import Navigation from "./Components/Navigation/Navigation.js";
-import "./Components/Navigation/Navigation.css";
 import Card from "./Components/Card/Card.js";
-import "./Components/Card/Card.css";
 import "./Components/Body/Body.css";
 
 function App() {
   return (
     <div className="App">
-      <body>
-        <Header />
+      <Header />
+      <main>
         <Card />
         <Card />
         <Card />
-        <Navigation />
-      </body>
+      </main>
+      <Navigation />
     </div>
   );
 }
